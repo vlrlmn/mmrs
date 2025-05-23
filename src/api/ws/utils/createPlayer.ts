58f2661ea8d57261d8490f6f5f1f0fd71469dd06
vlrlmn <lomakina.valerie@gmail.com>
2025-05-23 14:09:20 +0400

    create player fix

--- src/api/ws/utils/createPlayer.ts
+++ src/api/ws/utils/createPlayer.ts
@@ -3,7 +3,7 @@ import { Player } from '../../../domain/matchmaking/types';
 export function createPlayer(id: string, mmr: number, socket: any): Player {
   return {
     id,
-    mmr: parseInt(mmr.toString(), 10),
+    mmr,
     socket,
     joinedAt: Date.now()
   };
